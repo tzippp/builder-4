0c7986682bc8f7c3af1b31941c8b33e526209aa1
zippy <tzipporamiller@gmail.com>
2025-06-04 19:51:17 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React from 'react';
 
-export default function App() {
-  return <div>Hello from SweetPeasGirl Builder!</div>;
+function App() {
+  return <div>Hello from Builder-ready React App!</div>;
 }
+
+export default App;
